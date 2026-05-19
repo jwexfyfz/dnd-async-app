61f456e83a85c8a800b6940e125465a4968b4721
jeffwang <jwexfyfz@gmail.com>
2026-05-18 21:18:59 -0700

    testing hello world 4

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
